06f054261493548a580e946c7daac77a51dee6c5
sandeshjadhav5 <jadhavs932@gmail.com>
2023-05-25 00:36:23 +0530

    pulled

--- client/src/Pages/MarkAttendendance.jsx
+++ client/src/Pages/MarkAttendendance.jsx
@@ -34,38 +34,44 @@ const MarkAttendendance = () => {
     lecture_type: "",
   });
   const dispatch = useDispatch();
-  const [subjectArr, setSubjectArr] = useState([])
-  const [subject, setSubject] = useState(null)
-  const [studentsData, setStudentsData] = useState([])
-  const [present, setPresent] = useState([])
-  
-  useEffect(()=>{
-    getSubjects()
-    getStudents()
-  },[])
-  const getSubjects = async()=>{
-    try{
-      let res = await axios.get("https://long-gray-cougar-toga.cyclic.app/subjects");
-      setSubjectArr(res.data)
-    }catch(err){
-      throw err
-    }
-  }
-  const getStudents = async()=>{
-    try{
-      let res = await axios.get("https://long-gray-cougar-toga.cyclic.app/students")
-      setStudentsData(res.data)
-    }catch(err){
+  const [subjectArr, setSubjectArr] = useState([]);
+  const [subject, setSubject] = useState(null);
+  const [studentsData, setStudentsData] = useState([]);
+  const [present, setPresent] = useState([]);
 
+  useEffect(() => {
+    getSubjects();
+    if (data.year && subject) {
+      getStudents();
     }
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data.year, subject?.subject_id]);
+  const getSubjects = async () => {
+    try {
+      let res = await axios.get(
+        "https://long-gray-cougar-toga.cyclic.app/subjects"
+      );
+      setSubjectArr(res.data);
+    } catch (err) {
+      throw err;
+    }
+  };
+  console.log(studentsData);
+  const getStudents = async () => {
+    try {
+      let res = await axios.get(
+        `https://long-gray-cougar-toga.cyclic.app/students?year=${data.year}&subject=${subject?.subject_id}`
+      );
+      setStudentsData(res.data);
+    } catch (err) {}
+  };
   const handleSubmitAddStudent = (e) => {
     e.preventDefault();
-    if(present.length){
-      const payload = {...data,...subject,present:present}
-      dispatch(addStudents(payload));
-    }else{
-      alert("Lecture cannot be without any attendence !")
+    if (present.length) {
+      const payload = { ...data, ...subject, present: present };
+      dispatch(addLecture(payload));
+    } else {
+      alert("Lecture cannot be without any attendence !");
     }
   };
 
@@ -98,102 +104,124 @@ const MarkAttendendance = () => {
   return (
     <Box w={"100%"}>
       <SidebarHeader />
-      <div className="mainContent">
-        <Box w={"80%"} m={"auto"} textAlign={"left"} p={10}>
-          <Text textAlign="left" fontSize="1.5rem" p="2" fontWeight="2rem">
-            Create New Lecture
-          </Text>
-          <form onSubmit={handleSubmitAddStudent} className="modalForm">
-            <div>
-              <label>Lecture Date :</label>
-              <br />
-              <input
-                onChange={handleChange}
-                name="lecture_date"
-                type="date"
-                required
-                value={data.lecture_date}
-              />
-            </div>
+      <Box
+        w={"60%"}
+        m={"auto"}
+        textAlign={"left"}
+        className="mainContent"
+        p={10}
+      >
+        <Text textAlign="left" fontSize="1.5rem" p="2" fontWeight="2rem">
+          Create New Lecture
+        </Text>
+        <form onSubmit={handleSubmitAddStudent} className="modalForm">
+          <div>
+            <label>Lecture Date :</label>
+            <br />
+            <input
+              onChange={handleChange}
+              name="lecture_date"
+              type="date"
+              required
+              value={data.lecture_date}
+            />
+          </div>
 
-            <div>
-              <label>Lecture Start Time :</label>
-              <br />
-              <input
-                name="start_at"
-                onChange={handleChange}
-                type="time"
-                required
-                value={data.start_at}
-              />
-            </div>
+          <div>
+            <label>Lecture Start Time :</label>
+            <br />
+            <input
+              name="start_at"
+              onChange={handleChange}
+              type="time"
+              required
+              value={data.start_at}
+            />
+          </div>
 
-            <div>
-              <label>Lecture End Time :</label>
-              <br />
-              <input
-                onChange={handleChange}
-                type="time"
-                name="end_at"
-                required
-                value={data.end_at}
-              />
-            </div>
+          <div>
+            <label>Lecture End Time :</label>
+            <br />
+            <input
+              onChange={handleChange}
+              type="time"
+              name="end_at"
+              required
+              value={data.end_at}
+            />
+          </div>
 
           <div>
             <label>Select Year :</label> <br />
             <select name="year" onChange={handleChange} required>
-              <option value="" >Select Year</option>
+              <option value="">--select--</option>
               <option value={"First"}>First</option>
               <option value={"Second"}>Second</option>
-              <option value={"Third"} >Third</option>
-              <option value={"Fourth"} >Fourth</option>
+              <option value={"Third"}>Third</option>
+              <option value={"Fourth"}>Fourth</option>
             </select>
           </div>
 
           <div>
             <label>Lecture Type :</label> <br />
-            <input type="text" name="lecture_type" value={data.lecture_type} onChange={handleChange} placeholder="Theory, Pratical..." />
+            <select name="lecture_type" required onChange={handleChange}>
+              <option value="">--select type--</option>
+              <option value="Theory">Theory</option>
+              <option value="Practical">Practical</option>
+            </select>
+            {/* <input type="text" name="" value={data.lecture_type} onChange={handleChange} placeholder="Theory, Pratical..." /> */}
           </div>
 
           <div>
-            <label >Select Lecture Subject :</label>
+            <label>Select Lecture Subject :</label>
             <select name="subject" onChange={handleSubjectChange}>
-              <option value="">--select Subject--</option>   
-            {subjectArr.map((el,id)=><option key={id} value={el._id}>{el.name}</option>)}
+              <option value="">--select Subject--</option>
+              {subjectArr.map((el, id) => (
+                <option key={id} value={el._id}>
+                  {el.name}
+                </option>
+              ))}
             </select>
           </div>
-          <Box border={2} >
+          <Box border={2}>
             <TableContainer>
-            <Table size="sm" variant="striped" colorScheme="purple">
-              <TableCaption>List of All Students Enrolled</TableCaption>
-              <Thead>
-                <Tr>
-                  <Th>Name</Th>
-                  <Th>Registration Number</Th>
-                  <Th>Roll Number</Th>
-                  <Th>Present</Th>
-                </Tr>
-              </Thead>
-              <Tbody>
-                {studentsData.map((el) => (
-                  <Tr key={el._id}>
-                    <Td>{el.name}</Td>
-                    <Td> {el.registrationNumber}</Td>
-                    <Td>{el.year}</Td>
-                    <Td>
-                    <input onChange={(e)=>handlePresent(el._id,e)} type="checkbox" />
-                    </Td>
+              <Table size="sm" variant="striped" colorScheme="purple">
+                <TableCaption>List of All Students Enrolled</TableCaption>
+                <Thead>
+                  <Tr>
+                    <Th>Name</Th>
+                    <Th>Registration Number</Th>
+                    <Th>Roll Number</Th>
+                    <Th>Present</Th>
                   </Tr>
-                ))}
-              </Tbody>
-              <Tfoot>
-                <Tr>
-                  <Th>Total={studentsData.length}</Th>
-                </Tr>
-              </Tfoot>
-            </Table>
-          </TableContainer>
+                </Thead>
+                <Tbody>
+                  {studentsData.length === 0 && (
+                    <Tr textAlign={"center"} color={"gray.500"}>
+                      Please Select Lecture and Year to Get Students
+                    </Tr>
+                  )}
+                  {studentsData.map((el) => (
+                    <Tr key={el._id}>
+                      <Td>{el.name}</Td>
+                      <Td> {el.registrationNumber}</Td>
+                      <Td>{el._id}</Td>
+                      <Td>
+                        <input
+                          onChange={(e) => handlePresent(el._id, e)}
+                          type="checkbox"
+                        />
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+                <Tfoot>
+                  <Tr>
+                    <Th>Total={studentsData.length}</Th>
+                  </Tr>
+                </Tfoot>
+              </Table>
+            </TableContainer>
           </Box>
           <input className="submitBtnAdmin" type="submit" />
         </form>
